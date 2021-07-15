5c924cc95530d6dfba50700e838e80410fc3c86b
machadoams <alexandresmmarcel@gmail.com>
2021-07-14 21:19:03 -0300

    fix typos

--- R/setup.R
+++ R/setup.R
@@ -1,22 +1,23 @@
 # SETUP
 
 ### Install/Load packages required ----
-if(!require(tidyverse)){install.packages("tidyverse)");library(tidyverse)}
+if(!require(tidyverse)){install.packages("tidyverse");library(tidyverse)}
 if(!require(car)){install.packages("car");library(car)}
 if(!require(glmmTMB)){install.packages("glmmTMB");library(glmmTMB)}
 if(!require(DHARMa)){install.packages("DHARMa");library(DHARMa)}
 if(!require(MASS)){install.packages("MASS");library(MASS)}
 if(!require(MuMIn)){install.packages("MuMIn");library(MuMIn)}
 if(!require(performance)){install.packages("performance");library(performance)}
-if(!require(partR2)){install.packages("partR2)");library(partR2)}
-if(!require(future)){install.packages("future)");library(future)}
-if(!require(lme4)){install.packages("lme4)");library(lme4)}
-if(!require(patchwork)){install.packages("patchwork)");library(patchwork)}
-if(!require(gghalves)){install.packages("gghalves)");library(gghalves)}
-if(!require(colorblindr)){install.packages("colorblindr)");library(colorblindr)}
-if(!require(sjPlot)){install.packages("sjPlot)");library(sjPlot)}
-if(!require(jtools)){install.packages("jtools)");library(jtools)}
-if(!require(cowplot)){install.packages("cowplot)");library(cowplot)}
+if(!require(partR2)){install.packages("partR2");library(partR2)}
+if(!require(future)){install.packages("future");library(future)}
+if(!require(lme4)){install.packages("lme4");library(lme4)}
+if(!require(patchwork)){install.packages("patchwork");library(patchwork)}
+if(!require(gghalves)){install.packages("gghalves");library(gghalves)}
+if(!require(colorblindr)){install.packages("colorblindr");library(colorblindr)}
+if(!require(sjPlot)){install.packages("sjPlot");library(sjPlot)}
+if(!require(jtools)){install.packages("jtools");library(jtools)}
+if(!require(cowplot)){install.packages("cowplot");library(cowplot)}
+if(!require(bookdown)){install.packages("bookdown");library(bookdown)}
 
 
 ### Function to get same number of breaks ----
